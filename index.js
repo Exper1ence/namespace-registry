50b5feb75a00f19fed4910a12c2d89a411994c9a
Exper1ence <3533357503@qq.com>
2016-12-13 18:56:27 +0800

    update

--- index.js
+++ index.js
@@ -14,10 +14,15 @@ const n = function () {
 
 n.register = (obj) => {
     if (!_.isPlainObject(obj))throw new TypeError('the first argument must be a plain object');
-    _.forOwn(obj, (value, key) => {
-        if (namespaceObject[key])throw new Error(`the key '${key}' has already be registered.`);
-        namespaceObject[key] = value;
-    });
+    try {
+        _.forOwn(obj, (value, key) => {
+            if (namespaceObject[key])throw 100;
+            namespaceObject[key] = value;
+        });
+    }
+    catch (e) {
+        if (e == 100)throw new Error(`the key '${key}' has already be registered.`);
+    }
 };
 
 module.exports = n;
