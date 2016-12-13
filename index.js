5505b97570ea2653d986955ecb4bebd042a5b2e9
Exper1ence <3533357503@qq.com>
2016-12-13 19:08:59 +0800

    update

--- index.js
+++ index.js
@@ -3,25 +3,34 @@
  */
 const _ = require('lodash');
 
+class NError {
+    constructor(code, value) {
+        this.code = code;
+        this.value = value;
+    }
+}
 const namespaceObject = {};
-const n = function () {
+function n() {
     const cb = Array.prototype.pop.call(arguments);
     const wants = Array.prototype.map.call(arguments, (want) => {
         return namespaceObject[want];
     });
     return cb.apply(null, wants);
-};
+}
 
 n.register = (obj) => {
     if (!_.isPlainObject(obj))throw new TypeError('the first argument must be a plain object');
     try {
         _.forOwn(obj, (value, key) => {
-            if (namespaceObject[key])throw 100;
+            if (namespaceObject[key])throw new NError(100, key);
             namespaceObject[key] = value;
         });
     }
     catch (e) {
-        if (e == 100)throw new Error(`the key '${key}' has already be registered.`);
+        if (e instanceof NError) {
+            if (e.code == 100)throw new Error(`the key '${e.value}' has already be registered.`);
+        }
+        else throw e;
     }
 };
 
